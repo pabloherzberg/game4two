6d9d5d50c978ab9279ff07803e5f15be0f2eb9d7
pablo.herzberg <pablo.herzberg@ltm.digital>
2022-03-01 16:44:44 -0300

    fix get players

--- src/helpers/database/index.js
+++ src/helpers/database/index.js
@@ -1,8 +1,13 @@
 import init from "../firebaseConfig";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 
 init();
 const db = getDatabase();
+
+export function updatePlayers() {
+  const playersRef = ref(db, "players");
+  onValue(playersRef, (snapshot) => Object.values(snapshot.val()));
+}
 
 export function updatePlayer(player) {
   const playersRef = ref(db, `players/${player.uid}`);
--- src/store/players/index.js
+++ src/store/players/index.js
@@ -1,12 +1,22 @@
 import { defineStore } from "pinia";
 import init from "../../helpers/firebaseConfig";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 init();
+const db = getDatabase();
 
 export const usePlayersStore = defineStore("players", {
   state: () => {
     return {
       players: [],
     };
+  },
+  actions: {
+    updatePlayers() {
+      const playersRef = ref(db, "players");
+      onValue(playersRef, (snapshot) => {
+        this.players = Object.values(snapshot.val());
+      });
+    },
   },
 });
